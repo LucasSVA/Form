f61888509c689884601e2125db1cc2a542867301
LucasSVA <lucas.svanadze@gmail.com>
2022-07-18 22:31:39 +0200

    bootstrap css styles for valid and invalid

--- src/App.js
+++ src/App.js
@@ -8,36 +8,64 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
+      firstName: "",
+      lastName: "",
       email: "",
       password: "",
       rememberMe: false,
       emailIsValid: false,
       passwordIsValid: false,
       isSubmitted: false,
-      firstName: "",
-      lastName: ""
+      Namevalide: "form-control is-invalid",
+      SureNameValide: "form-control is-invalid",
+      validEmail: "form-control is-invalid",
+      validPassword: "form-control is-invalid",
     }
 
   }
 
   handleFirstNameChange = (e) => {
     this.setState({
-      firstName: e.target.value
+      firstName: e.target.value,
+      Namevalide: "form-control is-valid"
     })
+
+    if (e.target.value === "") {
+      this.setState({ Namevalide: "form-control is-invalid" })
+    }
 
   }
   handleLastNameChange = (e) => {
-    this.setState({ lastName: e.target.value })
+    this.setState({
+      lastName: e.target.value,
+      SureNameValide: "form-control is-valid"
+    })
+    if (e.target.value === "") {
+      this.setState({ SureNameValide: "form-control is-invalid" })
+    }
+
   }
 
 
-
-
   handleEmailChange = (e) => {
+
     this.setState({
       email: e.target.value,
-      emailIsValid: (regex.test(e.target.value))
+      emailIsValid: (regex.test(e.target.value)),
+
     })
+
+    if (regex.test(e.target.value) === true) {
+      this.setState({
+        validEmail: "form-control is-valid"
+      })
+    } else {
+      this.setState({
+        validEmail: "form-control is-invalid"
+      })
+    }
+
+
   }
 
   handlePasswordChange = (e) => {
@@ -46,15 +74,18 @@ class App extends React.Component {
     })
     if (this.state.password.length >= 5) {
       this.setState({
-        passwordIsValid: true
+        passwordIsValid: true,
+        validPassword: "form-control is-valid"
       })
     } else {
       this.setState({
         passwordIsValid: false
       })
     }
-    console.log(this.state.password)
-    console.log(this.state.passwordIsValid)
+    if (e.target.value.length < 6) {
+      this.setState({ validPassword: "form-control is-invalid" })
+    }
+
   }
 
   handleRememberMeChange = () => {
@@ -64,10 +95,12 @@ class App extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault()
     if (this.state.emailIsValid = true && this.state.passwordIsValid) {
-      this.setState({ isSubmitted: true })
+      this.setState({
+        isSubmitted: true
+      })
 
     }
-    console.log(this.state.isSubmitted);
+
   }
 
 
@@ -77,34 +110,40 @@ class App extends React.Component {
 
     return (
       <>
-        {this.state.isSubmitted ? (<h1>seggt</h1>) : (
+        {this.state.isSubmitted ? (
+          <div className="success">
+            <span className="submitted">Submitted</span>
+            <span className="first-last">{this.state.firstName} {this.state.lastName}</span>
+            <span className="validEmail">{this.state.email}</span>
+
+          </div>
+        ) : (
 
           <div className="container ">
-            <form onSubmit={this.handleSubmit}>
+
+            <span className="login">LogIn</span>
+            <form onSubmit={this.handleSubmit} novalidate>
 
               <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label" onChange={this.handleFirstNameChange}>Name</label>
-                <input type="text" className="form-control" onChange={this.handlePasswordChange} />
+                <label htmlFor="exampleInputPassword1" className="form-label" >First Name</label>
+                <input type="text" className={this.state.Namevalide} onChange={this.handleFirstNameChange} />
               </div>
 
               <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label" onChange={this.handleLastNameChange}>Name</label>
-                <input type="text" className="form-control" onChange={this.handlePasswordChange} />
+                <label htmlFor="exampleInputPassword1" className="form-label" >Last Name</label>
+                <input type="text" className={this.state.SureNameValide} onChange={this.handleLastNameChange} />
               </div>
 
               <div className="mb-3">
                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={this.state.email} onChange={this.handleEmailChange} />
+                <input type="email" className={this.state.validEmail} id="exampleInputEmail1" aria-describedby="emailHelp" value={this.state.email} onChange={this.handleEmailChange} />
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
               </div>
 
               <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" onChange={this.handlePasswordChange} />
+                <label htmlFor="exampleInputPassword1" >Password</label>
+                <input type="password" className={this.state.validPassword} id="exampleInputPassword1" onChange={this.handlePasswordChange} />
               </div>
-
-
-
 
               <div className="mb-3 form-check">
                 <input type="checkbox" className="form-check-input" id="exampleCheck1" onChange={this.handleRememberMeChange} />
